0799ae6284ab451f5439e47f13eeb9d73296c64c
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-14 11:36:34 -0700

    prework calculator completed

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,7 +6,100 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1) Addition");
+            Console.WriteLine("2) Subtraction");
+            Console.WriteLine("3) Multiplication");
+            Console.WriteLine("4) Division");
+            Console.WriteLine("5) Exit");
+            Console.WriteLine("");
+            Console.Write("Choose an option: ");
+            string result = Console.ReadLine();
+
+            if (result == "1")
+            {
+                Add();
+                return true;
+            }
+            else if (result == "2")
+            {
+                Subtract();
+                return true;
+            }
+            else if (result == "3")
+            {
+                Multiply();
+                return true;
+            }
+            else if (result == "4")
+            {
+                Divide();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static void Add()
+        {
+            Console.Clear();
+            Console.WriteLine("To add numbers, please enter two numbers below.");
+            Console.WriteLine("");
+            Console.Write("number a: ");
+            decimal a = Decimal.Parse(Console.ReadLine());
+            Console.Write("number b: ");
+            decimal b = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.ReadLine();
+        }
+        private static void Subtract()
+        {
+            Console.Clear();
+            Console.WriteLine("To subtract numbers, please enter two numbers below.");
+            Console.WriteLine("");
+            Console.Write("number a: ");
+            decimal a = Decimal.Parse(Console.ReadLine());
+            Console.Write("number b: ");
+            decimal b = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            Console.ReadLine();
+        }
+        private static void Multiply()
+        {
+            Console.Clear();
+            Console.WriteLine("To multiply numbers, please enter two numbers below.");
+            Console.WriteLine("");
+            Console.Write("number a: ");
+            decimal a = Decimal.Parse(Console.ReadLine());
+            Console.Write("number b: ");
+            decimal b = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            Console.ReadLine();
+        }
+        private static void Divide()
+        {
+            Console.Clear();
+            Console.WriteLine("To divide numbers, please enter two numbers below.");
+            Console.WriteLine("");
+            Console.Write("number a: ");
+            decimal a = Decimal.Parse(Console.ReadLine());
+            Console.Write("number b: ");
+            decimal b = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -3,10 +3,22 @@
 ## Prework Calculator
 Create a very basic, 4 function calculator within a console application.
 
+Upon completion, your calculator should contain the following:
+
+Inside the Program.cs file of your console application you should have 4 external methods plus the Main method:
+* Add()
+* Subtract()
+* Multiply()
+* Divide()
+Each of these methods should take in 2 parameters, and return the solution of those 2 numbers. (i.e if you are adding 2 numbers than your return would be the sum of the 2 numbers added together. )
+
+Output to the console the answer to the proposed math problem.
+
 ### Author: Kyungrae Kim
 
 ### Links and Resources
 * https://canvas.instructure.com/courses/1737674/assignments/12794982
+* https://docs.microsoft.com/en-us/dotnet/api/system.decimal.parse?view=netframework-4.8
 
 ### Setup
 * This challenge will be completed via Visual Studio Community Version
